b0bbf9fa349c9dfe8f8ab4e702c964489bca599c
Reto Hablützel <rethab@protonmail.ch>
2020-12-22 21:02:13 +0100

    more tests for closures in the vm

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -774,6 +774,53 @@ mod tests {
             ),
             int(11)
         );
+        assert_eq!(
+            run_vm_test(
+                "
+                    let newAdderOuter = fn(a, b) {
+                        let c = a + b;
+                        fn(d) {
+                            let e = d + c;
+                            fn(f) { e + f; };
+                        };
+                    };
+                    let newAdderInner = newAdderOuter(1, 2)
+                    let adder = newAdderInner(3);
+                    adder(8);
+                "
+            ),
+            int(14)
+        );
+        assert_eq!(
+            run_vm_test(
+                "
+                    let a = 1;
+                    let newAdderOuter = fn(b) {
+                        fn(c) {
+                            fn(d) { a + b + c + d };
+                        };
+                    };
+                    let newAdderInner = newAdderOuter(2)
+                    let adder = newAdderInner(3);
+                    adder(8);
+                "
+            ),
+            int(14)
+        );
+        assert_eq!(
+            run_vm_test(
+                "
+                let newClosure = fn(a, b) {
+                    let one = fn() { a; };
+                    let two = fn() { b; };
+                    fn() { one() + two(); };
+                };
+                let closure = newClosure(9, 90);
+                closure();
+                "
+            ),
+            int(99)
+        );
     }
 
     fn compile(input: &str) -> Compiler {
